53c47388bc13e6a6385bd3a0dbf626f106c29b26
Dwake5 <wakeling101@hotmail.com>
2019-05-17 17:19:34 +0100

    added pokemon

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -13,33 +13,33 @@ function addTrainers (trainers) {
     trainers.forEach(addTrainer)
 }
 
-// function getPokemons() {
-// 	return fetch(POKEMONS_URL)
-// 		.then((resp) => resp.json())
-// }
-
-// function addPokemons (pokemons) {
-//     pokemons.forEach()
-// }
-
 function addTrainer (trainer) {
     // const pokemons = getPokemons().then(pokemons => pokemons.filter(pokeList))
     // function pokeList(pokemon) {
     //     return pokemon.trainer_id == trainer.id;
     // }
-    // debugger
+    
+
     const card = document.createElement('div')
+    const list = document.createElement('ul')
+    function addPokemon (pokemons) {
+            pokemons.forEach(pokemon => {
+                list.innerHTML += `
+                    <li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>
+                    `
+            })
+    }
     card.className = 'card' 
     card.innerHTML = `
     <p> ${trainer.name} </p>
     <button data-trainer-id="${trainer.id}">Add Pokemon</button>
-    <ul>
-   <li> </li>
-    </ul>
     `
-
+    // debugger
+    addPokemon(trainer.pokemons)
     mainBody.appendChild(card)
+    card.appendChild(list)
 }
+
 
 document.addEventListener('DOMContentLoaded', event => {
     getTrainers()
